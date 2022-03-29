66c276989e09d15822e6edffcf5ed5e593bb2fe5
Deolu_Josef <kiishijoseph@gmail.com>
2022-03-29 05:03:25 +0100

    added a custom button, form and sign in component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import HomePage from './pages/homepage/homepage.component';
 
 import ShopPage from './pages/shop/shop.component';
 
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
+
 import Header from './components/header/header.component';
 
 import './App.css';
@@ -17,6 +19,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
+        <Route path="/signin" element={<SignInAndSignUpPage />} />
       </Routes>
     </div>
   );
