b5309168b44cd034ab0e8be68dcde24f84a9a40f
Deolu_Josef <kiishijoseph@gmail.com>
2022-04-05 19:12:19 +0100

    included sign in with email and password

--- src/App.js
+++ src/App.js
@@ -36,11 +36,10 @@ class App extends React.Component {
               ...snapShot.data(),
             },
           });
-          console.log(this.state)
         });
       }
 
-      this.setState({currentUser: userAuth})
+      this.setState({ currentUser: userAuth });
     });
   }
 
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,7 +4,7 @@ import FormInput from '../form-input/form-input.component';
 
 import CustomButton from '../custom-button/custom-button';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -18,10 +18,17 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
 
-    this.setState({ email: '', password: '' });
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   handleChange = (event) => {
